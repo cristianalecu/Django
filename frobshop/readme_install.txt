1d3f942d73c1b640bc605d49b2660d5a9e619632
Cristian Alecu <cristian.alecu@gmail.com>
2017-08-25 15:53:22 +0300

    django-oscar 0

---
+++ frobshop/readme_install.txt
@@ -0,0 +1,17 @@
+pip install django-oscar
+pip install django_extensions
+rem pip install debug_toolbar
+rem pip install template_timings_panel
+pip install south
+pip install rosetta
+rem pip install scipy
+pip install django_compressor
+pip install django_apps
+pip install Whoosh
+pip install pycountry
+
+
+
+
+
+
--- ordersite/settings.py
+++ ordersite/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'item',
     'order',
     'orderitem',
@@ -79,8 +78,10 @@ WSGI_APPLICATION = 'ordersite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MainDB',
+        'USER': 'admin',
+        'PASSWORD': 'go4admin',
     }
 }
 
