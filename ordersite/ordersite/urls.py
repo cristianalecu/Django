0d783050c9ec79b20ab3a509e574f3cadda14de2
Cristian Alecu <cristian.alecu@gmail.com>
2017-08-31 01:32:22 +0300

    login 1

--- ordersite/ordersite/urls.py
+++ ordersite/ordersite/urls.py
@@ -1,35 +1,11 @@
-an for the column groups 
-		var groups = this.s.groups;
-		var hungarianGroups = this.s.aoGroups;
-		if ( groups ) {
-			for ( var i=0, ien=groups.length ; i<ien ; i++ ) {
-				if ( groups[i].title ) {
-					hungarianGroups[i].sTitle = groups[i].title;
-				}
-				if ( groups[i].columns ) {
-					hungarianGroups[i].aiColumns = groups[i].columns;
-				}
-			}
-		}
-	},
-
-
-	/**
-	 * On each table draw, check the visibility checkboxes as needed. This allows any process to
-	 * update the table's column visibility and ColVis will still be accurate.
-	 *  @method  _fnDrawCallback
-	 *  @returns void
-	 *  @private
-	 */
-	"_fnDrawCallback": function ()
-	{
-		var columns = this.s.dt.aoColumns;
-		var buttons = this.dom.buttons;
-		var groups = this.s.aoGroups;
-		var button;
-
-		for ( var i=0, ien=buttons.length ; i<ien ; i++ ) {
-			button = buttons[i];
-
-			if ( button.__columnIdx !== undefined ) {
-				$('input', butt
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from order import views
+ 
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('blog.urls')),
+    #url(r'^', include('orders.urls')),
+    url(r'^$', views.home, name='home'),
+ ]
