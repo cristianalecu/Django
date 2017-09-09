faf2889ef3d1bb393228fdd5182876cf61788648
Cristian Alecu <cristian.alecu@gmail.com>
2017-09-09 14:25:03 +0300

    accoun urls

--- DjangoByExample/src/account/urls.py
+++ DjangoByExample/src/account/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login, logout, logout_then_login, password_change, password_change_done, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+
+
 from . import views
 
 
@@ -10,19 +13,19 @@ urlpatterns = [
     url(r'^edit/$', views.edit, name='edit'),
 
     # login / logout urls
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
 
     # change password urls
-    url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
-    url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
+    url(r'^password-change/$', password_change, name='password_change'),
+    url(r'^password-change/done/$', password_change_done, name='password_change_done'),
 
     # restore password urls
-    url(r'^password-reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
-    url(r'^password-reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
-    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
-    url(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+    url(r'^password-reset/$', password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
 
     # user profiles
     url(r'^users/$', views.user_list, name='user_list'),
